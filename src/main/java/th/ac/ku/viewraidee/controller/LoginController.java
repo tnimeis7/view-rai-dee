c4adcbef61c984099e03c5710ec6863f105b80e8
Pear-chanita <60770328+Pear-chanita@users.noreply.github.com>
2021-10-06 22:19:49 +0700

    add login2

--- src/main/java/th/ac/ku/viewraidee/controller/LoginController.java
+++ src/main/java/th/ac/ku/viewraidee/controller/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @RequestMapping("/login")
-    public String getLoginPage(Model model){
+    public String getLoginPage(Model model , @AuthenticationPrincipal OAuth2User principal){
         return "login";
     }
 
