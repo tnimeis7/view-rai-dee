a47be547511f0b3cc349fc104230deea74ab7a65
tnimeis7 <isreeya.n@ku.th>
2021-10-08 13:16:16 +0700

    edit security config of logout error

--- src/main/java/th/ac/ku/viewraidee/config/SecurityConfig.java
+++ src/main/java/th/ac/ku/viewraidee/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         .and()
                 .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
                 .logoutSuccessUrl("/").permitAll();
 
     }
