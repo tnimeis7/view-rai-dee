3288604ce591dea6052244186dc2b00f1ef75119
Pear-chanita <60770328+Pear-chanita@users.noreply.github.com>
2021-10-14 00:32:00 +0700

    edit icon show/hide password

--- src/main/resources/static/js/password-check.js
+++ src/main/resources/static/js/password-check.js
@@ -2,11 +2,12 @@ function checkField() {
     password = document.signUp.password.value;
     confirmPassword = document.signUp.confirmPassword.value;
 
-    if (password != confirmPassword) {
-        alert ("\nรหัสผ่านไม่ตรง กรุณาตรวจสอบใหม่อีกครั้ง")
+    if (password == null || confirmPassword == null) {
+        alert ("\nกรอกข้อมูลไม่ครบ กรุณาตรวจสอบใหม่อีกครั้ง")
         return false;
-    }else if(password == null && confirmPassword == null){
-        alert("\nกรอกข้อมูลไม่ครบ กรุณาตรวจสอบใหม่อีกครั้ง")
+    }else if(password != confirmPassword){
+        alert("\nรหัสผ่านไม่ตรง กรุณาตรวจสอบใหม่อีกครั้ง")
+        return false;
     }
     else{
         return true;
