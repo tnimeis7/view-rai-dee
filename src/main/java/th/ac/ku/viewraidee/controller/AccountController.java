be0eba6f95cef4ed2851247a990a6cd8f80ff11e
tnimeis7 <isreeya.n@ku.th>
2021-10-13 17:47:31 +0700

    add edit field

--- src/main/java/th/ac/ku/viewraidee/controller/AccountController.java
+++ src/main/java/th/ac/ku/viewraidee/controller/AccountController.java
@@ -1,11 +1,13 @@
 package th.ac.ku.viewraidee.controller;
 
+import org.apache.tomcat.util.http.fileupload.FileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,14 +22,18 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    @GetMapping("/edit")
+    public String getEditAccountPage(Model model) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Account account = accountService.getById(username);
+        model.addAttribute("account", account);
+        return "edit-account";
+    }
+
     @PostMapping("/edit")
     public String editAccountPage(Model model, Account account, RedirectAttributes redirectAttributes, @RequestParam("image") MultipartFile multipartFile) throws Exception {
-        if(!multipartFile.isEmpty()) {
-            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-            account.setPhoto(fileName);
-            Account saveAccount = accountService
-        }
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("YESSSS");
         return "edit-account";
     }
 
