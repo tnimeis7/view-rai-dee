d4a4735b719c4015ba849acc1e9253a76155b123
Pear-chanita <60770328+Pear-chanita@users.noreply.github.com>
2021-10-13 16:13:32 +0700

    add edit-account.html

--- src/main/java/th/ac/ku/viewraidee/controller/AccountController.java
+++ src/main/java/th/ac/ku/viewraidee/controller/AccountController.java
@@ -1,11 +1,34 @@
 package th.ac.ku.viewraidee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import th.ac.ku.viewraidee.model.Account;
+import th.ac.ku.viewraidee.service.AccountService;
 
 @Controller
-@RequestMapping("/Account")
+@RequestMapping("/account")
 public class AccountController {
+    @Autowired
+    private AccountService accountService;
 
+    @PostMapping("/edit")
+    public String editAccountPage(Model model, Account account, RedirectAttributes redirectAttributes, @RequestParam("image") MultipartFile multipartFile) throws Exception {
+        if(!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            account.setPhoto(fileName);
+            Account saveAccount = accountService
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "edit-account";
+    }
 
 }
