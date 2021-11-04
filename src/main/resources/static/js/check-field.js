bf319982a8701134e27a762c73cdb7cf214c6f9e
Pear-chanita <60770328+Pear-chanita@users.noreply.github.com>
2021-11-05 01:11:18 +0700

    change password alert

--- src/main/resources/static/js/check-field.js
+++ src/main/resources/static/js/check-field.js
@@ -7,10 +7,39 @@ function checkFieldSignUp() {
     if(username == "" || email == "" || password == "" || confirmPassword == "") {
         alert("\nกรอกข้อมูลไม่ครบ กรุณาตรวจสอบใหม่อีกครั้ง")
         return false;
-    }else if(password !== confirmPassword){
+    }else if(password !== confirmPassword) {
         alert ("\nรหัสผ่านไม่ตรง กรุณาตรวจสอบใหม่อีกครั้ง")
         return false;
     }else{
+        return true;
+    }
+
+}
+
+function checkFieldLogin() {
+    username = document.login.username.value;
+    password = document.login.password.value;
+
+    if(username == "" || password == "") {
+        alert("\nกรอกข้อมูลไม่ครบ กรุณาตรวจสอบใหม่อีกครั้ง")
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function checkPassword() {
+    current = document.getElementById("currentPasswordField");
+    password = document.getElementById("passwordField");
+    confirm = document.getElementById("confirmPasswordField");
+
+    if(current == "" || password == "" || confirm == "") {
+        alert("กรุณากรอกข้อมูลให้ครบ");
+        return false;
+    }else if(password != confirm) {
+        alert("รหัสผ่านไม่ตรงกัน โปรดตรวจสอบอีกครั้ง");
+        return false;
+    }else {
         return true;
     }
 
