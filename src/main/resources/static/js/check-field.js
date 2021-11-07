9e31f5e16e1d2fe67c6863ebed495794a14c9609
Pear-chanita <60770328+Pear-chanita@users.noreply.github.com>
2021-11-07 11:48:53 +0700

    change password alert

--- src/main/resources/static/js/check-field.js
+++ src/main/resources/static/js/check-field.js
@@ -29,9 +29,13 @@ function checkFieldLogin() {
 }
 
 function checkPassword() {
-    current = document.getElementById("currentPasswordField");
-    password = document.getElementById("passwordField");
-    confirm = document.getElementById("confirmPasswordField");
+    current = document.changePassword.currentPassword.value;
+    password = document.changePassword.password.value;
+    confirm = document.changePassword.confirmPassword.value;
+
+    console.log(current);
+    console.log(password);
+    console.log(confirm);
 
     if(current == "" || password == "" || confirm == "") {
         alert("กรุณากรอกข้อมูลให้ครบ");
